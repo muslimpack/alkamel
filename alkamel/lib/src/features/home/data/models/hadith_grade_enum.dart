9a88c8f8b74ab8b065add031456c816ad39f6069
7eltantawy <hasaneltantawy@gmail.com>
2024-10-17 22:01:14 +0300

    Update hadith_grade_enum.dart

--- alkamel/lib/src/features/home/data/models/hadith_grade_enum.dart
+++ alkamel/lib/src/features/home/data/models/hadith_grade_enum.dart
@@ -1,30 +1,68 @@
 import 'package:flutter/material.dart';
 
+///   والحديث الصحيح مراتبه هي (صحيح، صحيح لغيره، حسن، حسن لغيره)
+///   والحديث الضعيف (ضعيف، مرسل صحيح، مرسل حسن، مرسل ضعيف)
+///   والحديث المتروك (ضعيف جدا، مرسل ضعيف جدا)
+///   والحديث المكذوب (مكذوب)
+
 enum HadithGradeEnum {
+  athar(
+    title: "أثر",
+    color: Colors.green,
+    lookupWords: [],
+    lookupOrder: 4,
+  ),
+
   authentic(
-    "صحيح",
-    Colors.green,
-    ["صحيح", "صحيح لغيره", "حسن", "حسن لغيره"],
+    title: "صحيح",
+    color: Colors.green,
+    lookupWords: ["صحيح", "حسن"],
+    lookupOrder: 3,
   ),
   weak(
-    "ضعيف",
-    Colors.yellow,
-    ["ضعيف", "مرسل صحيح", "مرسل حسن", "مرسل ضعيف"],
+    title: "ضعيف",
+    color: Colors.yellow,
+    lookupWords: ["ضعيف", "مرسل"],
+    lookupOrder: 2,
   ),
   abandoned(
-    "متروك",
-    Colors.orange,
-    ["ضعيف جدا", "مرسل ضعيف جدا"],
+    title: "متروك",
+    color: Colors.orange,
+    lookupWords: ["ضعيف جد"],
+    lookupOrder: 1,
   ),
   fabricated(
-    "مكذوب",
-    Colors.red,
-    ["مكذوب"],
+    title: "مكذوب",
+    color: Colors.red,
+    lookupWords: ["مكذوب"],
+    lookupOrder: 0,
   ),
   ;
 
-  const HadithGradeEnum(this.title, this.color, this.dbNames);
-  final List<String> dbNames;
+  const HadithGradeEnum({
+    required this.title,
+    required this.color,
+    required this.lookupWords,
+    required this.lookupOrder,
+  });
+
+  static HadithGradeEnum getFromString(String gradeText) {
+    HadithGradeEnum? hadithGradeEnum;
+    for (final grade in HadithGradeEnum.values.reversed) {
+      if (hadithGradeEnum != null) break;
+      for (final dbG in grade.lookupWords) {
+        if (gradeText.contains(dbG)) {
+          hadithGradeEnum = grade;
+          break;
+        }
+      }
+    }
+
+    return hadithGradeEnum ?? HadithGradeEnum.athar;
+  }
+
+  final List<String> lookupWords;
   final String title;
   final Color color;
+  final int lookupOrder;
 }
