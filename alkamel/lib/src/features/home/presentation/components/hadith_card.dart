afafec11306b95a1da4f51ae42d4df2b39189cb8
7eltantawy <hasaneltantawy@gmail.com>
2024-10-17 23:27:03 +0300

    Update hadith_card.dart

--- alkamel/lib/src/features/home/presentation/components/hadith_card.dart
+++ alkamel/lib/src/features/home/presentation/components/hadith_card.dart
@@ -1,10 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:math';
-
-import 'package:alkamel/src/core/shared/text_highlighter.dart';
-import 'package:alkamel/src/features/home/data/models/hadith_grade_enum.dart';
+import 'package:alkamel/src/features/home/presentation/components/hadith_card_popup_menu.dart';
+import 'package:alkamel/src/features/home/presentation/components/responsive_text.dart';
 import 'package:alkamel/src/features/search/data/models/hadith.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class HadithCard extends StatelessWidget {
@@ -20,9 +17,6 @@ class HadithCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final HadithGradeEnum hadithGradeEnum =
-        HadithGradeEnum.getFromString(hadith.grade);
-
     return Card(
       margin: margin,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
@@ -30,7 +24,7 @@ class HadithCard extends StatelessWidget {
         decoration: BoxDecoration(
           border: Border(
             right: BorderSide(
-              color: hadithGradeEnum.color.withOpacity(.3),
+              color: hadith.gradeEnum.color.withOpacity(.3),
               width: 7,
             ),
           ),
@@ -51,6 +45,7 @@ class HadithCard extends StatelessWidget {
                       style: Theme.of(context).textTheme.bodyMedium,
                     ),
                   ),
+                  HadithCardPopupMenu(hadith: hadith),
                 ],
               ),
               const Divider(),
@@ -64,9 +59,9 @@ class HadithCard extends StatelessWidget {
               ),
               const Divider(),
               Text(
-                "المرتبة: ${hadith.grade}  |  الحكم: [${hadithGradeEnum.title}]",
+                "المرتبة: ${hadith.grade}  |  الحكم: [${hadith.gradeEnum.title}]",
                 style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                      color: hadithGradeEnum.color,
+                      color: hadith.gradeEnum.color,
                       fontWeight: FontWeight.bold,
                     ),
               ),
@@ -75,87 +70,5 @@ class HadithCard extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-class ResponsiveText extends StatefulWidget {
-  final String text;
-  final String? searchedText;
-  final TextStyle? style;
-  const ResponsiveText(
-    this.text, {
-    super.key,
-    this.style,
-    this.searchedText,
-  });
-
-  @override
-  State<ResponsiveText> createState() => _ResponsiveTextState();
-}
-
-class _ResponsiveTextState extends State<ResponsiveText> {
-  late bool expanded;
-  bool isLong = true;
-  final int length = 280;
-
-  @override
-  void initState() {
-    isLong = widget.text.length > length;
-    expanded = !isLong;
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    /// bodyText
-    final bodyText = expanded
-        ? widget.text
-        : widget.text.substring(0, min(widget.text.length, length));
-
-    final bodyTextSpan = HighlightText(
-      text: bodyText,
-      textToHighlight: widget.searchedText ?? "",
-      highlightStyle: widget.style?.copyWith(
-        color: Theme.of(context).colorScheme.primary,
-      ),
-      style: widget.style,
-    ).textSpan();
-
-    /// show more
-    final bool showMoreText = !expanded && widget.text.length > length;
-
-    final moreTextSpan = TextSpan(
-      text: " ...المزيد",
-      style: const TextStyle(color: Colors.blue),
-      recognizer: TapGestureRecognizer()
-        ..onTap = () {
-          setState(() {
-            expanded = true;
-          });
-        },
-    );
-
-    ///
-    final textWidget = Text.rich(
-      TextSpan(
-        children: [
-          bodyTextSpan,
-          if (showMoreText) moreTextSpan,
-        ],
-      ),
-    );
-
-    if (isLong) {
-      return GestureDetector(
-        onTap: () {
-          setState(() {
-            expanded = !expanded;
-          });
-        },
-        child: textWidget,
-      );
-    }
-
-    return textWidget;
   }
 }
