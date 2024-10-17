852cb1f3ed3311babfadb520a22efde617a62644
7eltantawy <hasaneltantawy@gmail.com>
2024-10-17 23:26:56 +0300

    Create share_dialog.dart

---
+++ alkamel/lib/src/features/share/presentation/components/share_dialog.dart
@@ -0,0 +1,94 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:alkamel/generated/l10n.dart';
+import 'package:alkamel/src/core/extensions/extension.dart';
+import 'package:alkamel/src/core/functions/show_toast.dart';
+import 'package:alkamel/src/features/search/data/models/hadith.dart';
+import 'package:alkamel/src/features/share/presentation/screens/share_as_image_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:share_plus/share_plus.dart';
+
+Future showShareDialog(BuildContext context, {required Hadith hadith}) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return ShareDialog(hadith: hadith);
+    },
+  );
+}
+
+class ShareDialog extends StatelessWidget {
+  final Hadith hadith;
+  const ShareDialog({
+    super.key,
+    required this.hadith,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final StringBuffer sb = StringBuffer();
+    sb.write(hadith.rawy);
+    if (hadith.rawyReference.isNotEmpty) sb.write("(${hadith.rawyReference})");
+    sb.write("\n\n-------\n\n");
+    sb.write(hadith.hadith);
+    sb.write("\n\n-------\n\n");
+    sb.write("المرتبة: ${hadith.grade}  |  الحكم: [${hadith.gradeEnum.title}]");
+
+    final shareText = sb.toString();
+
+    return ConstrainedBox(
+      constraints: const BoxConstraints(minWidth: 200),
+      child: AlertDialog(
+        scrollable: true,
+        title: Text(S.of(context).share),
+        content: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Card(
+              child: Container(
+                constraints: const BoxConstraints(maxHeight: 350),
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.all(10),
+                  child: Text(shareText),
+                ),
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            tooltip: S.of(context).shareAsImage,
+            icon: const Icon(Icons.camera_alt_outlined),
+            onPressed: () {
+              context.push(
+                ShareAsImageScreen(
+                  hadith: hadith,
+                ),
+              );
+            },
+          ),
+          IconButton(
+            tooltip: S.of(context).copy,
+            icon: const Icon(Icons.copy),
+            onPressed: () async {
+              await Clipboard.setData(
+                ClipboardData(text: shareText),
+              );
+              showToast(
+                msg: S.current.copiedToClipboard,
+              );
+            },
+          ),
+          IconButton(
+            tooltip: S.of(context).share,
+            icon: const Icon(Icons.share),
+            onPressed: () async {
+              await Share.share(shareText);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
