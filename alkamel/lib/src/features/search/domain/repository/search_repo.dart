ba18f3515b85580cf9948ac9372b2e81504d0dcc
7eltantawy <hasaneltantawy@gmail.com>
2024-10-19 05:04:15 +0300

    SearchRepo add searchCollectionsFilters

--- alkamel/lib/src/features/search/domain/repository/search_repo.dart
+++ alkamel/lib/src/features/search/domain/repository/search_repo.dart
@@ -1,3 +1,4 @@
+import 'package:alkamel/src/features/home/data/models/hadith_collection_enum.dart';
 import 'package:alkamel/src/features/home/data/models/hadith_ruling_enum.dart';
 import 'package:hive/hive.dart';
 
@@ -6,6 +7,7 @@ class SearchRepo {
 
   SearchRepo(this.box);
 
+  /// ruling filters
   static const String searchRulingFiltersKey = "searchRulingFilters";
   List<HadithRulingEnum> get searchRulingFilters {
     final data = box.get(searchRulingFiltersKey) as String?;
@@ -28,6 +30,33 @@ class SearchRepo {
   ) async {
     return box.put(
       searchRulingFiltersKey,
+      rulingSearchFilters.map((x) => x.id).join(","),
+    );
+  }
+
+  /// collections filters
+  static const String searchCollectionsFiltersKey = "searchCollectionsFilters";
+  List<HadithCollectionEnum> get searchCollectionsFilters {
+    final data = box.get(searchCollectionsFiltersKey) as String?;
+    if (data == null) return HadithCollectionEnum.values;
+    try {
+      final splitted = data.split(",").map(
+            (id) => HadithCollectionEnum.values
+                .where((r) => r.id.toString() == id)
+                .first,
+          );
+
+      return splitted.toList();
+    } catch (e) {
+      return HadithCollectionEnum.values;
+    }
+  }
+
+  Future setSearchCollectionsFilters(
+    List<HadithCollectionEnum> rulingSearchFilters,
+  ) async {
+    return box.put(
+      searchCollectionsFiltersKey,
       rulingSearchFilters.map((x) => x.id).join(","),
     );
   }
